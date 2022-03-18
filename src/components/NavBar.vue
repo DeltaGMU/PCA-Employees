67477659239da4adce1df77e1353baf790cacdc4
dimitrikjohnson <dimitrik.johnson@gmail.com>
2022-03-18 02:58:23 -0400

    added a navbar dropdown and styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,12 +2,26 @@
   <div>
 
     <span class="pcaNav" v-if="isLoggedIn">
-      <p class="text-white">Signed in as {{getName}}</p>
-      <a class="text-white" id="signOut" @click="logout">Sign Out</a>
+      <div class="centerInDiv">
+        <p class="text-white">Signed in as {{getName}}</p>
+      </div>
+      <div class="centerInDiv">
+        <div class="dropdown">
+          <div class="dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <!--<p class="profileBox">{{getInitials}}</p>-->
+            <p class="profileBox"><i class="fas fa-user fa-2xl"></i></p>
+          </div>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Submit Leave Request</a></li>
+            <li><a class="dropdown-item" @click="logout">Sign Out</a></li>
+          </ul>
+        </div>
+        
+      </div>    
     </span>
 
     <span class="pcaNav" v-else>
-      <p class="text-white">Providence Christian Academy</p>
+      <p class="text-white navSection">Providence Christian Academy</p>
     </span>
 
   </div>
@@ -24,6 +38,11 @@ export default {
     },
     getName: function() {
       return this.$store.getters.StateName;
+    },
+    getInitials: function() {
+      let fullName = this.$store.getters.StateName.split(' ');
+      let initials = fullName.shift().charAt(0) + fullName.pop().charAt(0);
+      return initials.toUpperCase();
     },
     ...mapGetters({Name: "StateName"}),
   },
