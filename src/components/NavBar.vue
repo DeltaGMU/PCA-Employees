d99070ce9dd6fed165ff0ee1bf77783f8c353ffa
dimitrikjohnson <dimitrik.johnson@gmail.com>
2022-03-20 16:35:31 -0400

    navbar dropdown box checks for current page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,11 +12,11 @@
             <i class="dropdown-caret fa-solid fa-angle-down" ></i>
           </div>
           <ul class="dropdown-menu">
-            <li v-if=" getRole != 'administrator' "><a class="dropdown-item" href="#">Submit Leave Request</a></li>
+            <li v-if=" getRole != 'administrator' && getCurrentPage() == '/timesheet' "><a class="dropdown-item" @click="goToLeaveRequest">Submit Leave Request</a></li>
+            <li v-if=" getRole != 'administrator' && getCurrentPage() == '/leaverequest' "><a class="dropdown-item" @click="goToTimesheet">Submit Timesheet</a></li>
             <li><a class="dropdown-item" @click="logout">Sign Out</a></li>
           </ul>
         </div>
-        
       </div>    
     </span>
 
@@ -49,6 +49,15 @@ export default {
     async logout() {
       await this.$store.dispatch("LogOut");
       this.$router.push("/");
+    },
+    goToLeaveRequest() {
+      this.$router.push("/leaverequest");
+    },
+    goToTimesheet() {
+      this.$router.push("/timesheet");
+    },
+    getCurrentPage() {
+      return window.location.pathname
     },
     ...mapActions(["GetName"]),
     ...mapActions(["GetRole"]),
