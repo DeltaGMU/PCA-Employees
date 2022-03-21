d87ce253a14db45a9eb9dd52b362995889fe16bd
dimitrikjohnson <dimitrik.johnson@gmail.com>
2022-03-21 00:10:44 -0400

    finished basic form structure

--- src/views/LeaveRequest.vue
+++ src/views/LeaveRequest.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrapper">
-        <div class="topSection">
+        <div class="topSection noSelect">
 
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
 
@@ -12,26 +12,96 @@
         </div>
 
         <div>
-            <div class="mb-3 row">
-                <label for="name" class="text-blue">Employee Name</label>
+            <div class="row noSelect">
+                <div class="mb-1">
+                    <label for="name" class="text-blue formLabel leaveLabel">Employee Name</label>
+                </div>
+                
                 <input type="text" readonly class="form-control-plaintext leaveInput" id="name" :value=" getName ">
             </div>
-            <div class="mb-3 row">
-                <label for="currentDate" class="text-blue">Current Date</label>
+            <div class="row noSelect">
+                <div class="mb-1">
+                    <label for="currentDate" class="text-blue formLabel leaveLabel">Current Date</label>
+                </div>
+                
                 <input type="text" readonly class="form-control-plaintext leaveInput" id="currentDate" :value=" getCurrentDate() ">
             </div>
-             <div class="mb-3 row">
-                <label class="text-blue">Date(s) of Absence</label>
+            <div class="row">
+                <div class="mb-2">
+                    <label class="text-blue formLabel leaveLabel">Date(s) of Absence</label>
+                </div>
+                
                 <div class="mb-3 input-group">
-                    <input type="date" class="form-control" id="absenceDateStart" :min = " min ">
+                    <input type="date" class="form-control textBox" id="absenceDateStart" :min = " min " required>
                     <span class="input-group-text">to</span>
-                    <input type="date" class="form-control" id="absenceDateEnd" :min=" min ">
-                </div>        
+                    <input type="date" class="form-control textBox" id="absenceDateEnd" :min=" min " required>
+                </div>
+                <div class="mb-3 row">
+                    <div class="mb-1 col-auto">
+                        <label class="form-text"># of full days</label> <br />
+                        <input type="number" class="form-control textBox" id="fullDays">
+                    </div>
+                    <div class="mb-1 col-auto">
+                        <label class="form-text" for="halfDays"># of half days</label><br>
+                        <input type="number" class="form-control textBox" id="halfDays">
+                    </div>
+                    <div class="mb-1 col-auto">
+                        <label class="form-text" for="hours"># of hours (round to nearest 15 minutes)</label><br>
+                        <input type="number" class="form-control textBox" id="hours">
+                    </div>
+                </div>            
+            </div>
+            <div class="mb-2 row">
+                <label for="currentDate" class="text-blue formLabel leaveLabel">Reason for Absence</label>
+            </div>
+            <div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="illnessSelf" id="illnessSelf">
+                    <label class="form-check-label" for="illnessSelf">Illness - Self</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="illnessFamily" id="illnessFamily">
+                    <label class="form-check-label" for="illnessFamily">Illness - Family</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="deathInFamily" id="deathInFamily">
+                    <label class="form-check-label" for="deathInFamily">Death in the Family</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="personal" id="personal">
+                    <label class="form-check-label" for="personal">Personal</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="juryDuty" id="juryDuty">
+                    <label class="form-check-label" for="juryDuty">Jury Duty</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="reservesOrGuard" id="reservesOrGuard">
+                    <label class="form-check-label" for="reservesOrGuard">Reserves Or Guard</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="professional" id="professional">
+                    <label class="form-check-label" for="professional">Professional</label>
+                </div>
+                <div class="mb-3 form-check form-check-inline leaveInput">
+                    <input class="form-check-input textBox" type="checkbox" value="pcaRelated" id="pcaRelated">
+                    <label class="form-check-label" for="pcaRelated">PCA Related</label>
+                </div>
             </div>
             <div class="mb-3 row">
-                <input type="number">
+                <div class="mb-2">
+                    <label for="whoWillCover" class="text-blue formLabel leaveLabel">Who Will Cover the Absence, or How Will It Be Covered?</label>
+                </div>
+                <textarea id="whoWillCover" rows="5" class="form-control textBox leaveInput"></textarea>
+            </div>
+            <div class="mb-3 row">
+                <div class="mb-2">
+                    <label for="comments" class="text-blue formLabel leaveLabel">Comments</label>
+                </div>
+                <textarea id="comments" rows="5" class="form-control textBox leaveInput"></textarea>
             </div>
         </div>
+        <button class="btn blueBtn">Submit</button>
     </div>
 </template>
 
@@ -41,14 +111,10 @@ import { mapGetters, mapActions } from "vuex";
 export default {
     name: "LeaveRequest",
     data() {
-        const now = new Date()
-        const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
-        const minDate = new Date(today)
-
+        const now = new Date() 
         return {
-            value: "",
-            today: today,
-            min: minDate
+            today: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+            min: ""
         }
     },
     components: {},
@@ -63,7 +129,10 @@ export default {
 
             let day = ('0' + this.today.getDate()).slice(-2)
             let month = ('0' + (this.today.getMonth()+1)).slice(-2)
+
             let formattedDate = month + '/'  + day + '/' + this.today.getFullYear()
+
+            this.min = this.today.getFullYear() + '-'  + month + '-' + day
             return formattedDate
         },
         ...mapActions(["GetName"]),
