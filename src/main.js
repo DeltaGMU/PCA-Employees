7659cab147d0514066b7b8775e33270b4f9fb9f0
Jason Jerome <jasonjerome16@gmail.com>
2022-03-22 19:54:38 -0400

    Updated axios base endpoint to the HTTPS python server

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
-import https from 'https'
+// import https from 'https'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -11,8 +11,13 @@ import '@fortawesome/fontawesome-free/js/all.js';
 import "./css/style.css";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://jjservers.ddns.net:56709/";
-axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false })
+axios.defaults.baseURL = "https://jjservers.ddns.net:56709/";
+/*
+axios.defaults.httpsAgent = new https.Agent({ 
+  rejectUnauthorized: false,
+  requestCert: true,
+})
+*/
 
 axios.interceptors.response.use(undefined, function(error) {
   if (error) {
