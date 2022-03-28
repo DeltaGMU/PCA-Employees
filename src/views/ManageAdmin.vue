2c1fd1ddaaa6f5226c7e9a26f0ab51b1f9511cf1
Jason Jerome <jasonjerome16@gmail.com>
2022-03-28 04:21:30 -0400

    Fixed ManageAdmin page formatting

--- src/views/ManageAdmin.vue
+++ src/views/ManageAdmin.vue
@@ -1,75 +1,67 @@
 <template>
-    <div class="row">
-        <div class="col-2 col-sm-3 col-md-4 col-lg-3 col-xxl-2">
+    <div class="d-flex">
+        <div class="p-2">
             <Sidebar>   </Sidebar>
         </div>
 
-        <div class = "mt-3 col-10 col-sm-9 col-md-8 col-xxl-10 pe-3 pe-md-4 pe-lg-4 pe-xl-5">
+        <div class = "p-2 flex-grow-1">
             <form>
                 <div>
                     <div class="topSection noSelect">
-
-                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-
+                        <div class="mb-1">
                             <h1 class="text-blue">Manage My Account</h1>
-
                         </div>
-                    
                     </div>
 
                     <div>
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="firstName" class="text-blue formLabel">First Name</label>
                             </div>
-                            
-                            <input type="text" class="form-control leaveInput" id="firstName" required>
-                            
+                            <input type="text" class="form-control" id="firstName" required>
                         </div>
 
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="lastName" class="text-blue formLabel">Last Name</label>
                             </div>
                             
-                            <input type="text" class="form-control leaveInput" id="lastName" required>
+                            <input type="text" class="form-control" id="lastName" required>
                         </div>
 
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="passwordChange" class="text-blue formLabel">Password</label><br>
                                 <button type="button" class="mt-2 btn blueBtn" id="passwordChange">Change Password</button>
                             </div>                    
                         </div>
 
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="primaryEmail" class="text-blue formLabel">Primary Email</label>
                             </div>
-                            
-                            <input type="text" class="form-control leaveInput" id="primaryMail">
+                            <input type="text" class="form-control" id="primaryMail">
                         </div>
                     
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="secondaryEmail" class="text-blue formLabel leaveLabel">Secondary Email</label>
                             </div>
-                            
-                            <input type="text" class="form-control leaveInput" id="secondaryEmail" placeholder="Optional...">
+                            <input type="text" class="form-control" id="secondaryEmail" placeholder="Optional...">
                         </div>
 
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="notification" class="text-blue formLabel">Enable email notifications for primary email?</label>
                             </div>
                             
-                            <div class="form-check form-check-inline leaveInput">
+                            <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="flexRadioDefault1" id="notification flexRadioDefault1" checked>
                                 <label class="form-check-label" for="flexRadioDefault1">
                                     Yes
                                 </label>
                                 </div>
-                                <div class="form-check form-check-inline leaveInput">
+                                <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="flexRadioDefault1" id="notification flexRadioDefault1">
                                 <label class="form-check-label" for="flexRadioDefault1">
                                     No
@@ -77,18 +69,18 @@
                             </div>
                         </div>
 
-                        <div class="form-group row noSelect">
+                        <div class="form-group noSelect">
                             <div class="mb-1">
                                 <label for="notification" class="text-blue formLabel">Enable email notifications for secondary email?</label>
                             </div>
                             
-                            <div class="form-check form-check-inline leaveInput">
+                            <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="flexRadioDefault2" id="notification flexRadioDefault2">
                                 <label class="form-check-label" for="flexRadioDefault2">
                                     Yes
                                 </label>
-                                </div>
-                                <div class="form-check form-check-inline leaveInput">
+                            </div>
+                            <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="flexRadioDefault2" id="notification flexRadioDefault2" checked>
                                 <label class="form-check-label" for="flexRadioDefault2">
                                     No
