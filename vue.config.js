b2cd34e4b2e3df9567d20a3c38f3b892650f60df
Jason Jerome <jasonjerome16@gmail.com>
2022-04-02 20:58:51 -0400

    [Experimental] Added self-signed certs to web interface

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
+var fs = require('fs');
+
 module.exports = {
     devServer: {
       host: '0.0.0.0',
       port: 443,
       https: true,
-      disableHostCheck: true
+      disableHostCheck: true,
+      key: fs.readFileSync('src/certs/key.pem', 'utf-8'),
+      cert: fs.readFileSync('src/certs/server.crt', 'utf-8'),
     }
 };
